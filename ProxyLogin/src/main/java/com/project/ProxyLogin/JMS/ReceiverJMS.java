1ffab830b7032bc7a8a72dc349b0e4a38b541021
gdr-98 <giuseppe.derosa20@studenti.unina.it>
2021-10-05 17:14:22 +0200

    Not necessary

--- ProxyLogin/src/main/java/com/project/ProxyLogin/JMS/ReceiverJMS.java
+++
@@ -1,8 +0,0 @@
-package com.project.ProxyLogin.JMS;
-/*
- * Login Proxy responds for unsuccessful login attempts.
- * Not implemented because not necessary now.
- */
-public class ReceiverJMS {
-
-}
