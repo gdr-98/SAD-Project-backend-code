7ec1cae22d4db22b65757f2ae393cef8a4d17c9d
gdr-98 <giuseppe.derosa20@studenti.unina.it>
2021-10-05 11:06:06 +0200

    Merge main

--- Proxy/src/main/java/com/project/Proxy/ProxyLogin/JMS/ReceiverJMS.java
+++
@@ -1,8 +0,0 @@
-package com.project.Proxy.ProxyLogin.JMS;
-/*
- * Login Proxy responds for unsuccessful login attempts.
- * Not implemented because not necessary now.
- */
-public class ReceiverJMS {
-
-}
